79052c41d5c9adfb7540f4ae5caaf745c7baa968
Mr_Cheese <kevinprovost776@gmail.com>
2019-04-29 16:39:01 -0500

    JS fixes

--- server.js
+++ server.js
@@ -23,4 +23,7 @@ var routes = require("./controllers/burgers_controllers");
 app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
-server.listen(process.env.PORT || PORT);
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
